e097b71a435cc03cde64a47fd2caf0a9e559127f
imcalledgautam <161356925+imcalledgautam@users.noreply.github.com>
2025-02-15 20:18:44 -0600

    Delete utils.py
    Structuring the project

--- utils.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv, find_dotenv
-
-def load_env_vars():
-    _ = load_dotenv(find_dotenv())
-    OPENAI_API_KEY = getpass()
-    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
